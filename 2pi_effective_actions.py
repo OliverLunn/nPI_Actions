8013d0a3b51044b5d6152a56d002727e3ad8c64c
unknown <ppyol1@nottingham.ac.uk>
2024-04-22 10:56:30 +0100

    all code good

--- 2pi_effective_actions.py
+++ 2pi_effective_actions.py
@@ -5,7 +5,7 @@ from matplotlib import colormaps
 from mpl_toolkits.mplot3d import Axes3D
 
 def integrand(phi, m, xhi, lamb, j, k):
-    integrand = np.exp(-(m / 2) * (phi ** 2) - (1/3) * xhi * (phi**3) - (lamb/24) * (phi ** 4) + (j * phi) + (k * phi**2))
+    integrand = np.exp(-(m / 2) * (phi ** 2) - (1/3) * xhi * (phi**3) - (lamb/24) * (phi ** 4) + (j * phi) + (1/2 * k * phi**2))
     return integrand
 
 def integration(j_values, k_values, m, xhi, lamb, z):
@@ -21,7 +21,7 @@ def gamma_j(phi, j_values, k_values, w, delta, array):
     """
     for j in range(len(j_values)):
             for k in range(len(k_values)):
-                array[k,j] = w[k,j] + j_values[j] * phi + k_values[k] * (delta)
+                array[k,j] = w[k,j] + j_values[j] * phi + 1/2 * k_values[k] * (phi**2 + delta)
     return array
 
 def maximise(g):
@@ -41,9 +41,9 @@ def maximise(g):
 
 if __name__ == '__main__':
    
-    m, xhi, lamb = -2, 0, 6
-    phi, delta = 2,2
-    step, min_val, max_val = 5, -50, 50
+    m, xhi, lamb = -2, 0, 4
+    phi, delta = 1,2
+    step, min_val, max_val = 0.1, -10, 10
     j_values = np.arange(min_val, max_val+step, step)                  
     k_values = np.arange(min_val, max_val+step, step)
     
@@ -57,12 +57,15 @@ if __name__ == '__main__':
     print(max_gamma)
 
     fig1, (ax1) = plt.subplots(1,1)
-
+    
     im1 = ax1.pcolormesh(j_values, k_values, g, cmap='viridis')
-    ax1.set_xlabel("J")
-    ax1.set_ylabel("K")
-    ax1.set_title("$\Gamma_{J,K}[$"+str(phi)+","+str(delta)+"$]$")
-    ax1.plot(j_values[max_coord[1]], k_values[max_coord[0]], ".k")
-    plt.colorbar(im1)
-    plt.tight_layout
+    ax1.set_xlabel("J", fontsize="30")
+    ax1.set_ylabel("K", fontsize="30")
+    ax1.set_title("$\Gamma_{J,K}[$"+str(phi)+","+str(delta)+"$]$", fontsize="30")
+    ax1.plot(j_values[max_coord[1]], k_values[max_coord[0]], ".k",markersize="12.5")
+    ax1.tick_params(labelsize=26)
+    cbar = plt.colorbar(im1)
+    cbar.ax.tick_params(labelsize=26)
+    ax1.set_aspect('equal')
+    plt.tight_layout()
     plt.show()
